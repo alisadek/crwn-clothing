3711d6833fa7be7fd3a39f0406339eeb5502b75f
alisadekk <alysadekk@gmail.com>
2022-01-20 21:05:14 +0200

    Added redux and implemented signin using redux

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+import store from "./redux/store";
+
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById("root"),
 );
